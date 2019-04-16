0383162cbdbce5579d8bb8d2044287ac0cfb4462
macos <macos@macoss-Mac.local>
2019-04-16 00:10:35 -0700

    add safe area for iOS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   Text,
   View,
   TouchableOpacity,
+  SafeAreaView
 }                           from 'react-native';
 import {
   RTCPeerConnection,
@@ -24,26 +25,26 @@ export default class App extends Component {
     mirror: false,
     objectFit: 'contain',
   };
-  
+
   async componentDidMount() {
     await this.initStream();
   }
-  
+
   initStream = async () => {
     try {
       // Get all devices (video/audio) in array list
       const sourceInfos = await mediaDevices.enumerateDevices();
       console.log(sourceInfos);
-      
+
       // Iterate the list above and find the front camera
       await Promise.all(sourceInfos.map(async sourceInfo => {
         console.log(sourceInfo);
-        
+
         if (sourceInfo.kind === 'videoinput' && sourceInfo.label === 'Camera 1, Facing front, Orientation 270') {
           this.setState({ videoSourceId: sourceInfo.deviceId });
         }
       }));
-      
+
       // Get the stream of front camera
       const stream = await mediaDevices.getUserMedia({
         audio: true,
@@ -57,23 +58,23 @@ export default class App extends Component {
           optional: [{ sourceId: this.state.videoSourceId }],
         },
       });
-      
+
       this.setState({ stream });
       console.log(stream);
     } catch (error) {console.log(error);}
   };
-  
+
   switchCamera = async () => {
-  
+
     const { stream } = this.state;
     stream.getVideoTracks().forEach(track => {
       track._switchCamera();
     });
-  
+
     /*this.setState({ isFront: !this.state.isFront });
      await this.initStream();*/
   };
-  
+
   objectFit = () => {
     if (this.state.objectFit === 'contain') {
       this.setState({ objectFit: 'cover' });
@@ -82,28 +83,30 @@ export default class App extends Component {
       this.setState({ objectFit: 'contain' });
     }
   };
-  
+
   button = (func, text) => (
     <TouchableOpacity style={s.button} onPress={func}>
       <Text style={s.buttonText}>{text}</Text>
     </TouchableOpacity>
   );
-  
+
   render() {
     const { stream, mirror, objectFit } = this.state;
-    
+
     return (
-      <View style={s.container}>
-        <RTCView
-          style={s.rtcView}
-          streamURL={stream.id}
-          mirror={mirror}
-          objectFit={objectFit}
-        />
-        {this.button(this.switchCamera, 'Change Camera')}
-        {this.button(() => this.setState({ mirror: !mirror }), 'Mirror')}
-        {this.button(this.objectFit, 'Object Fit (contain/cover)')}
-      </View>
+      <SafeAreaView style={{flex: 1}}>
+       <View style={s.container}>
+         <RTCView
+           style={s.rtcView}
+           streamURL={stream.id}
+           mirror={mirror}
+           objectFit={objectFit}
+         />
+         {this.button(this.switchCamera, 'Change Camera')}
+         {this.button(() => this.setState({ mirror: !mirror }), 'Mirror')}
+         {this.button(this.objectFit, 'Object Fit (contain/cover)')}
+       </View>
+      </SafeAreaView>
     );
   }
 }
